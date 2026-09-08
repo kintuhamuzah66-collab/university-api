7731bbcb0863ffd36e83c97045964308782fc028
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-09-08 08:03:50 +0300

    add allowed hosts

--- university/settings.py
+++ university/settings.py
@@ -27,8 +27,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "university-api-ahc8.onrender.com",
+]
 
 # Application definition
 
